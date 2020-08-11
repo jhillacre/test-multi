c8acca6a824f260049c4f699c8eee9cc446511d7
Joel Hillacre <joel@403forbidden.ca>
2020-08-11 15:59:27 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import ctypes
+import multiprocessing
+import random
+from time import sleep
+
+
+def init_worker(counter):
+    global _worker_id
+
+    with counter.get_lock():
+        counter.value += 1
+        _worker_id = counter.value
+
+def do_work(x):
+    print(_worker_id, 'starting', x)
+    sleep(random.uniform(1, 3))
+    print(_worker_id, 'ending', x)
+    return x
+
+
+if __name__ == '__main__':
+    counter = multiprocessing.Value(ctypes.c_int, 0)
+    pool = multiprocessing.Pool(
+        processes=8,
+        initializer=init_worker,
+        initargs=[counter])
+    test_results = pool.imap_unordered(do_work, (
+        x for x in range(32)
+    ))
+
+    while True:
+        try:
+            print('done', test_results.next(timeout=0.1))
+        except multiprocessing.TimeoutError:
+            continue
+        except StopIteration:
+            pool.close()
+            break
+
+    pool.join()
